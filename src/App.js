77435c9f0aba59508f4d69511c8309c4fb246a1d
Luciano Muniz <luciano.mo@gmail.com>
2022-06-17 16:56:55 -0300

    ajust movie route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='movies/:id' element={<Movie />} />
+      <Route path='/movies/:id' element={<Movie />} />
     </Routes>
   );
 };
